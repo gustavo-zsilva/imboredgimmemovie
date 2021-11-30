a1ccb229174aa74bf708a12fda03df67126eb4c6
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-11-30 20:39:29 -0300

    Made bubble responsive

--- src/components/Bubble.tsx
+++ src/components/Bubble.tsx
@@ -13,11 +13,24 @@ export function Bubble({ title, description }: BubbleProps) {
             borderRadius=".2rem"
             gridGap=".6rem"
             flexDir="column"
-            h="fit-content"
-            w="20rem"
             boxShadow="0 0 6px 6px rgba(0, 0, 0, 0.03)"
-            _odd={{ pb: '3rem' }}
-            _even={{ transform: 'translateY(-25%) translateX(35%)', bg: '#3c5fd4' }}
+            w={{
+                sm: "auto",
+                md: "20rem"
+            }}
+            h={{
+                sm: "100%",
+                md: "fit-content"
+            }}
+            _odd={{
+                sm: { pb: 'inherit' },
+                md: { pb: '3rem' }
+            }}
+            _even={{
+                sm: { bg: '#3c5fd4' },
+                md: { transform: 'translateY(-25%) translateX(15%)', bg: '#3c5fd4' },
+                lg: { transform: 'translateY(-25%) translateX(35%)', bg: '#3c5fd4' }
+            }}
         >
             <Text fontSize="1.2rem" fontWeight="bold">{title}</Text>
             <Text ml=".8rem">{description || `There are no ${title.toLowerCase()} currently.`}</Text>
