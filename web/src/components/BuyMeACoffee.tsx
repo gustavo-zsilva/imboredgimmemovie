ea5e87131a9f1fc20b3dc0e72b511182e81ee3ae
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-03 23:45:49 -0300

    Added BuyMeACoffee support link

---
+++ web/src/components/BuyMeACoffee.tsx
@@ -0,0 +1,28 @@
+import { BiCoffeeTogo } from 'react-icons/bi'
+import { Link, Button, Tooltip } from '@chakra-ui/react'
+
+export function BuyMeACoffee() {
+    return (
+        <Button
+            variant="ghost"
+            w="45px"
+            h="45px"
+            borderRadius="50%"
+        >
+            <Tooltip
+                label="Buy me a coffee!"
+                bg="dark.200"
+                color="primary.200"
+                mt=".4rem"
+            >
+                <Link
+                    href="https://www.buymeacoffee.com/gustavozx"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BiCoffeeTogo size={24} />
+                </Link>
+            </Tooltip>
+        </Button>
+    )
+}
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { MdOutlineMovie } from 'react-icons/md'
 
 import { Flex, Text } from "@chakra-ui/react";
+import { BuyMeACoffee } from './BuyMeACoffee';
 
 export function Header() {
     return (
-        <Flex gridArea="Header" alignItems="center">
+        <Flex gridArea="Header" alignItems="center" justifyContent="space-between">
             <Text fontWeight="bold" display="flex" alignItems="center" gridGap=".4rem">
                 imboredgimmemovie
                 <MdOutlineMovie />
             </Text>
+            <Flex>
+                <BuyMeACoffee />
+            </Flex>
         </Flex>
     )
 }
