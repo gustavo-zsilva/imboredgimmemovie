a6666cd8cc1b247914905eb3a9ddf29158fcc95e
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-03 23:11:50 -0300

    [Fix] Automatic language detection

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,24 +1,21 @@
 import axios from "axios";
 import { parseCookies } from 'nookies'
 
-const { '@ibgm_user_location': rawLocation } = parseCookies()
-const { locale } = rawLocation ? JSON.parse(rawLocation) : { locale: 'pt' }
-
-
 const apiKey = process.env.API_KEY
 
 export const api = axios.create({
     baseURL: `https://api.themoviedb.org/3`
 })
 
+if (typeof window !== undefined) {
 
-api.interceptors.request.use(config => {
+    const { '@ibgm_user_location': rawLocation } = parseCookies()
+    const { locale } = rawLocation
+    ? JSON.parse(rawLocation)
+    : { locale: 'us' }
 
-    
 
-    config.params = config.params || {}
-    config.params['api_key'] = apiKey
-    config.params['language'] = 'pt'
-
-    return config
-})
+    api.defaults.params = {}
+    api.defaults.params['api_key'] = apiKey
+    api.defaults.params['language'] = locale
+}
