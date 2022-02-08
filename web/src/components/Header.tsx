5e03ef45ac2868e334b29f140a129bbdaf5cff7a
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-07 23:17:54 -0300

    [Feature] Added sign in button

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { MdOutlineMovie } from 'react-icons/md'
 
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Button } from "@chakra-ui/react";
 import { BuyMeACoffee } from './BuyMeACoffee';
+import { useAuth } from '../hooks/useAuth';
 
 export function Header() {
+    const { signInWithGoogle } = useAuth()
+
     return (
         <Flex gridArea="Header" alignItems="center" justifyContent="space-between">
             <Text fontWeight="bold" display="flex" alignItems="center" gridGap=".4rem">
@@ -11,6 +14,9 @@ export function Header() {
                 <MdOutlineMovie />
             </Text>
             <Flex>
+                <Button onClick={signInWithGoogle}>
+                    Sign in with Google
+                </Button>
                 <BuyMeACoffee />
             </Flex>
         </Flex>
