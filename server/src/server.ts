c5445a34ad9ca678006b7638c93d566f16e87b75
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-14 19:54:13 -0300

    Added dynamic PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,10 @@ import cors from 'cors'
 import { router } from './routes'
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.listen(3001, () => console.log('Server open on port 3001.'))
+app.listen(PORT, () => console.log(`Server open on port ${PORT}.`))
