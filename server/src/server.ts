07007e9bc056fe581720ffb525eb9935eed11cba
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-14 16:32:41 -0300

    Fixed CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { router } from './routes'
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: true, credentials: true }))
+app.use(cors())
 app.use(router)
 
 app.listen(3001, () => console.log('Server open on port 3001.'))
