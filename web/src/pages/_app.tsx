527957c9a08e63f31f1e8d1a1b1382f49ed5a176
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-14 22:20:15 -0300

    [Fix] Fixed useLayoutEffect on SSR warnings

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from '../contexts/AuthContext'
 import { theme } from '../styles/global'
+
+// Fix useLayoutEffect on SSR warning
+if (typeof document === 'undefined') {
+    React.useLayoutEffect = React.useEffect;
+}
 
 function MyApp({ Component, pageProps }) {
   return (
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -14,6 +14,7 @@ import { MovieWatchProviders } from '../components/MovieWatchProviders'
 import { GenreList } from '../components/GenreList'
 import { Footer } from '../components/Footer'
 
+import axios from 'axios'
 import { parseCookies, setCookie } from 'nookies'
 import axios from 'axios'
 import { api } from '../services/api'
