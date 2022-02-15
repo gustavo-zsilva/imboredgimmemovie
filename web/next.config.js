d791efd7144a63320d7be133ef5cbb0f35744161
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-15 18:44:44 -0300

    [Optimization] Added Bundle Analyzer to analyze site bundle size

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: [
@@ -13,4 +17,4 @@ module.exports = {
     defaultLocale: 'us',
     locales: ['us', 'pt', 'fr', 'de'],
   }
-}
+})
