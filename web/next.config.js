b84162219e286991ef0cc68e5f53a6b1fb25e4a0
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-15 23:38:04 -0300

    Declared env variables

--- web/next.config.js
+++ web/next.config.js
@@ -12,6 +12,10 @@ module.exports = withBundleAnalyzer({
   },
   env: {
     API_KEY: process.env.API_KEY,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
   i18n: {
     defaultLocale: 'us',
