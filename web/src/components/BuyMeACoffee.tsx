227f7d4af2c761faa3b4d782b2fc345d4ae8fbf7
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-02-17 13:02:28 -0300

    [Fix] Fixed ghost click

--- web/src/components/BuyMeACoffee.tsx
+++ web/src/components/BuyMeACoffee.tsx
@@ -1,28 +1,24 @@
 import { BiCoffeeTogo } from 'react-icons/bi'
-import { Link, Button, Tooltip } from '@chakra-ui/react'
+import { Link, Tooltip } from '@chakra-ui/react'
 
 export function BuyMeACoffee() {
     return (
-        <Button
-            variant="ghost"
-            _hover={{ bg: "dark.600" }}
-            w="45px"
-            h="45px"
-            borderRadius="50%"
-            aria-label="Support Developer"
-        >
-            <Tooltip
-                label="Buy me a coffee!"
-                mt=".4rem"
+        <Tooltip label="Buy me a coffee!">
+            <Link
+                href="https://www.buymeacoffee.com/gustavozx"
+                target="_blank"
+                rel="noopener noreferrer"
+                _hover={{ bg: "dark.600" }}
+                w="45px"
+                h="45px"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                borderRadius="50%"
+                aria-label="Support Developer"
             >
-                <Link
-                    href="https://www.buymeacoffee.com/gustavozx"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <BiCoffeeTogo size={24} />
-                </Link>
-            </Tooltip>
-        </Button>
+                <BiCoffeeTogo size={24} />
+            </Link>
+        </Tooltip>
     )
 }
